cb7bb269741f9971342ced8aa951123fbb615570
treml <ftreml@gmail.com>
2018-05-01 23:47:48 +0200

    Directline3 API integration

--- index.js
+++ index.js
@@ -1,42 +1,78 @@
+const DirectLine = require('botframework-directlinejs').DirectLine
 const debug = require('debug')('botium-connector-directline3')
 
-global.XMLHttpRequest = require('xhr2');
+global.XMLHttpRequest = require('xhr2')
 
 class BotiumConnectorDirectline3 {
   constructor ({ queueBotSays, caps }) {
-   this.queueBotSays = queueBotSays
-   this.caps = caps
+    this.queueBotSays = queueBotSays
+    this.caps = caps
   }
 
   Validate () {
-   debug('Validate called')
-   return Promise.resolve()
+    debug('Validate called')
+    if (!this.caps['DIRECTLINE3_SECRET']) throw new Error('DIRECTLINE3_SECRET capability required')
+
+    return Promise.resolve()
   }
 
   Build () {
-   debug('Build called')
-   return Promise.resolve()
+    debug('Build called')
+    this.directLine = new DirectLine({
+      secret: this.caps['DIRECTLINE3_SECRET'],
+      webSocket: this.caps['DIRECTLINE3_WEBSOCKET'],
+      pollingInterval: this.caps['DIRECTLINE3_POLLINGINTERVAL']
+    })
+    return Promise.resolve()
   }
 
   Start () {
-   debug('Start called')
-   return Promise.resolve()
+    debug('Start called')
+    this.subscription = this.directLine.activity$
+      .filter(activity => activity.type === 'message' && activity.from.id !== 'me')
+      .subscribe(
+        message => {
+          debug('received message ', message)
+          const botMsg = { sender: 'bot', sourceData: message, messageText: message.text }
+          this.queueBotSays(botMsg)
+        }
+      )
+    return Promise.resolve()
   }
 
   UserSays (msg) {
-   debug('UserSays called, echo back')
-   const botMsg = { sender: 'bot', sourceData: msg.sourceData, messageText: msg.messageText }
-   this.queueBotSays(msg)
+    debug('UserSays called')
+    return new Promise((resolve, reject) => {
+      this.directLine.postActivity({
+        from: { id: msg.sender },
+        type: 'message',
+        text: msg.messageText
+      }).subscribe(
+        id => {
+          debug('Posted activity, assigned ID ', id)
+          resolve()
+        },
+        err => {
+          debug('Error posting activity', err)
+          reject(err)
+        }
+      )
+    })
   }
 
   Stop () {
-   debug('Stop called')
-   return Promise.resolve()
+    debug('Stop called')
+    if (this.subscription) {
+      this.subscription.unsubscribe()
+      this.subscription = null
+    }
+
+    return Promise.resolve()
   }
 
   Clean () {
-   debug('Clean called')
-   return Promise.resolve()
+    debug('Clean called')
+    return Promise.resolve()
   }
 }
 
